a2fe95822c72aac3b6a0beb2a0a4363ec1e4c2f1
Javier Andrés Villabón Morales <javavillabon@soy.sena.edu.co>
2024-07-02 18:49:27 -0500

    apollo client for graphql

--- src/main.js
+++ src/main.js
@@ -1,43 +1,58 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './style.css'
-
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-
-import FloatLabel from 'primevue/floatlabel';
-import InputNumber from 'primevue/inputnumber';
-import Button from 'primevue/button';
-import InputMask from 'primevue/inputmask';
-import InputText from 'primevue/inputtext';
-import Select from 'primevue/select';
-import Textarea from 'primevue/textarea';
+import { createApp, h } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./style.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client/core";
+import { createApolloProvider } from '@vue/apollo-option'
 
 
-const app = createApp(App)
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: 'http://localhost:4042/graphql',
+})
+
+
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
+
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+
+import FloatLabel from "primevue/floatlabel";
+import InputNumber from "primevue/inputnumber";
+import Button from "primevue/button";
+import InputMask from "primevue/inputmask";
+import InputText from "primevue/inputtext";
+import Select from "primevue/select";
+import Textarea from "primevue/textarea";
+
+const app = createApp({
+    render: () => h(App),
+});
 app.use(PrimeVue, {
-    theme: {
-        preset: Aura,
-        options: {
-            darkModeSelector: 'light',
-        }
-    }
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: "light",
+    },
+  },
 });
 
+app.component("InputNumber", InputNumber);
+app.component("FloatLabel", FloatLabel);
+app.component("Button", Button);
+app.component("InputMask", InputMask);
+app.component("InputText", InputText);
+app.component("Textarea", Textarea);
+app.component("Select", Select);
 
-app.component('InputNumber', InputNumber);
+app.use(router);
+app.use(apolloProvider);
 
-app.component('FloatLabel',FloatLabel)
-app.component('Button',Button)
-app.component('InputMask',InputMask)
-app.component('InputText',InputText)
-app.component('Textarea',Textarea)
-app.component('Select',Select)
-
-
-
-
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
