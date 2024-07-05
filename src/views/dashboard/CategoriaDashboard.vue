24949446134d04565bb1249270adef01a2ccc693
Javier Andrés Villabón Morales <javavillabon@soy.sena.edu.co>
2024-07-05 13:10:26 -0500

    :100:

--- src/views/dashboard/CategoriaDashboard.vue
+++ src/views/dashboard/CategoriaDashboard.vue
@@ -1,10 +1,31 @@
-<script  lang="ts" setup>
-import DashboardHeader from './DashboardHeader.vue';
-import DashboardAside from './DashboardAside.vue';
+<script setup lang="ts">
+import DashboardHeader from "./DashboardHeader.vue";
+import DashboardAside from "./DashboardAside.vue";
 </script>
 <template>
-<DashboardHeader />
-    <DashboardAside />
-
+    <div>
+    <DashboardHeader></DashboardHeader>
+    <div class="grid grid-cols-4 h-screen pt-5 bg-gray-100">
+      <DashboardAside></DashboardAside>
+      <div class="col-span-2 p-6 bg-white rounded-lg shadow-lg relative">
+        <h1 class="text-2xl font-semibold mb-6">Nuevo categoria</h1>
+        <form class="">
+          <div class="flex space-x-4">
+            <div class="w-1/2">
+              <label for="marca-vehiculo" class="block text-sm font-medium text-gray-700">Nombre categoria</label>
+              <Select  optionLabel="nombreMarcaVehiculo" placeholder="Nombre" class="mt-1 w-full border-gray-300 rounded-md shadow-sm" id="marca-vehiculo" />
+            </div>
+            <div>
+            <label for="cantidad" class="block text-sm font-medium text-gray-700">Descuento categoria</label>
+            <InputNumber inputId="minmax" :min="0" :max="100" id="cantidad" class="mt-1 w-full border-gray-300 rounded-md shadow-sm" />
+          </div>
+            <button type="submit" class=" border border-transparent rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              Agregar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped></style>
--- src/views/dashboard/ProductDashboard.vue
+++ src/views/dashboard/ProductDashboard.vue
@@ -1,37 +1,37 @@
-<script lang="ts">
-import DashboardHeader from './DashboardHeader.vue';
-import DashboardAside from './DashboardAside.vue';
+<script setup lang="ts">
+import DashboardHeader from "./DashboardHeader.vue";
+import DashboardAside from "./DashboardAside.vue";
+import { ref } from "vue";
+import gql from "graphql-tag";
+import { RouterLink } from "vue-router";
+import { useQuery } from "@vue/apollo-composable";
 
-export default {
-  name: 'ProductsList',
-  data() {
-    return {
-      products: []
-    }
-  },
-  methods: {
-    async getProducts() {
-      const response = await fetch('http://localhost:8090/api/v1/productos')
-      const json = await response.json()
-      this.products = json
-    },
-    async deleteProduct(id) {
-      const response = await fetch(`http://localhost:8090/api/v1/productos/${id}/delete`, {
-        method: 'DELETE'
-      })
-      if (response.ok) {
-        this.getProducts()
+let products = ref([]);
+
+const LOGIN_MUTATION = gql`
+  query {
+    products {
+      id
+      inventoryItem {
+        name
+        status
+        type
+        stock
       }
+      category {
+        discount
+      }
+      price
     }
-  },
-  components: {
-    DashboardHeader,
-    DashboardAside
-  },
-  beforeMount() {
-    this.getProducts()
   }
-}
+`;
+
+const { result, loading, error, onResult } = useQuery(LOGIN_MUTATION);
+
+onResult((queryResult) => {
+  products.value = queryResult.data
+  products.value = products.value.products
+});
 </script>
 <template>
   <div class="min-h-screen">
@@ -39,84 +39,135 @@ export default {
     <main>
       <div class="grid grid-cols-4 h-screen">
         <DashboardAside></DashboardAside>
-        <div class="col-span-3 m-5 row-span-3 shadow overflow-hidden rounded border-b border-gray-200  bg-white">
+        <div
+          class="col-span-3 m-5 row-span-3 shadow overflow-hidden rounded border-b border-gray-200 bg-white"
+        >
           <table class="">
             <thead class="">
               <tr>
-                <th class="text-gray-500  bg-gray-50 text-center align-middle uppercase">
-                  id </th>
+                <th
+                  class="text-gray-500 bg-gray-50 text-center align-middle uppercase"
+                >
+                  id
+                </th>
 
-                <th class="text-gray-500  bg-gray-50 text-center align-middle uppercase">
-                  nombre</th>
-                <th class="text-gray-500  bg-gray-50 text-center align-middle uppercase">
-                  estado</th>
+                <th
+                  class="text-gray-500 bg-gray-50 text-center align-middle uppercase"
+                >
+                  nombre
+                </th>
+                <th
+                  class="text-gray-500 bg-gray-50 text-center align-middle uppercase"
+                >
+                  estado
+                </th>
 
-                <th class="text-gray-500  bg-gray-50 text-center align-middle uppercase">
-                  descuento</th>
-                <th class="text-gray-500  bg-gray-50 text-center align-middle uppercase">
-                  tipo</th>
-                <th class="text-gray-500  bg-gray-50 text-center align-middle uppercase">
-                  cantidad</th>
-                <th class="text-gray-500  bg-gray-50 text-center align-middle uppercase">
-                  precio</th>
-                <th class="text-gray-500  bg-gray-50 text-center align-middle"></th>
-                <th class="text-gray-500  bg-gray-50 text-center align-middle"></th>
-                <th class="text-gray-500  bg-gray-50 text-center align-middle"></th>
+                <th
+                  class="text-gray-500 bg-gray-50 text-center align-middle uppercase"
+                >
+                  descuento
+                </th>
+                <th
+                  class="text-gray-500 bg-gray-50 text-center align-middle uppercase"
+                >
+                  tipo
+                </th>
+                <th
+                  class="text-gray-500 bg-gray-50 text-center align-middle uppercase"
+                >
+                  cantidad
+                </th>
+                <th
+                  class="text-gray-500 bg-gray-50 text-center align-middle uppercase"
+                >
+                  precio
+                </th>
+                <th
+                  class="text-gray-500 bg-gray-50 text-center align-middle"
+                ></th>
+                <th
+                  class="text-gray-500 bg-gray-50 text-center align-middle"
+                ></th>
+                <th
+                  class="text-gray-500 bg-gray-50 text-center align-middle"
+                ></th>
               </tr>
             </thead>
 
             <tbody class="bg-gray-50">
-
-
-
-              <tr v-for="product in products" :key="product.idProducto">
+              <tr v-for="product in products" :key="product.id">
                 <td class="align-middle">
                   <div>
-                    <p>{{ product.idProducto }}</p>
+                    <p>{{ product.id }}</p>
                   </div>
                 </td>
 
-                <td class="align-middle">{{ product.articulo.nombreArticulo }}</td>
                 <td class="align-middle">
-                  <button class="hover:bg-green-600 bg-green-600 font-bold text-white rounded-full">
-                    {{ product.articulo.estadoArticulo.nombreEstadoArticulo }}
-
+                  {{ product.inventoryItem.name }}
+                </td>
+                <td class="align-middle">
+                  <button
+                    class="hover:bg-green-600 bg-green-600 font-bold text-white rounded-full"
+                  >
+                    {{ product.inventoryItem.status }}
                   </button>
                 </td>
 
                 <td class="align-middle text-green-600 font-bold">
-                  {{product.categoria.descuentoCategoria}}%
+                  {{ product.category.discount }}%
                 </td>
                 <td class="align-middle">
-                  {{ product.articulo.estadoArticulo.nombreEstadoArticulo }}
+                  {{ product.inventoryItem.type }}
                 </td>
                 <td class="align-middle">
-                  <button class="hover:bg-orange-500 bg-orange-500 font-bold text-white rounded-full">
-                    {{ product.articulo.cantidad }}
+                  <button
+                    class="hover:bg-orange-500 bg-orange-500 font-bold text-white rounded-full"
+                  >
+                    {{ product.inventoryItem.stock }}
                   </button>
                 </td>
                 <td class="align-middle font-bold">
-                  {{ product.precio }}
+                  {{ product.price }}
                 </td>
                 <td class="align-middle">
-                  <RouterLink :to="{ name: 'updateproductoview', params: { id: product.idProducto } }"><button class="hover:bg-green-600 bg-green-700 font-bold text-white">Editar</button></RouterLink>
-
+                  <RouterLink
+                    :to="{
+                      name: 'updateproductoview',
+                      params: { id: product.id },
+                    }"
+                    ><button
+                      class="hover:bg-green-600 bg-green-700 font-bold text-white"
+                    >
+                      Editar
+                    </button></RouterLink
+                  >
                 </td>
                 <td class="align-middle">
-                  <button class="hover:bg-red-700 bg-red-500 font-bold text-white "  @click="deleteProduct(product.idProducto)">Eliminar</button>
+                  <button
+                    class="hover:bg-red-700 bg-red-500 font-bold text-white"
+                    @click="deleteProduct(product.id)"
+                  >
+                    Eliminar
+                  </button>
                 </td>
                 <td class="align-middle">
-                  <RouterLink :to="{ name: 'productoview', params: { id: product.idProducto } }"><button class="hover:bg-blue-500 bg-blue-700 font-bold text-white">Ver</button></RouterLink>
+                  <RouterLink
+                    :to="{
+                      name: 'productoview',
+                      params: { id: product.id },
+                    }"
+                    ><button
+                      class="hover:bg-blue-500 bg-blue-700 font-bold text-white"
+                    >
+                      Ver
+                    </button></RouterLink
+                  >
                 </td>
               </tr>
             </tbody>
           </table>
-
         </div>
-
       </div>
     </main>
-
   </div>
 </template>
-<style scoped lang="scss"></style>
