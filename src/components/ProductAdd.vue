df55643e66d0520652c44a58762cdcdedea18574
ivanti <139818368+tbrzc-sena@users.noreply.github.com>
2024-08-05 14:12:34 -0500

    create product is now functional
    ²²²²²

--- src/components/ProductAdd.vue
+++ src/components/ProductAdd.vue
@@ -4,6 +4,10 @@ import DashboardAside from "../views/dashboard/DashboardAside.vue";
 import gql from "graphql-tag";
 import { useQuery } from "@vue/apollo-composable";
 import { ref, computed, watch, onMounted, onBeforeMount } from "vue";
+import { useMutation } from "@vue/apollo-composable";
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
 
 const INFO_PRODUCT_QUERY = gql`
   query InfoForProductCreation {
@@ -26,6 +30,7 @@ const INFO_PRODUCT_QUERY = gql`
     discount
   }
   carTypes {
+    id
     name
   }
 }
@@ -53,7 +58,7 @@ mutation addProduct($imagen: String!, $precio: Int!, $nombre: String!, $cantidad
 }`
 
 let categoria = ref();
-let tipo = ref(["MAT","RAW","CUS"]);
+let tipo = ref(["MAT", "RAW", "CUS"]);
 let marca = ref();
 let modelo = ref();
 let tipoVehiculo = ref();
@@ -68,7 +73,7 @@ let producto = ref({
   modeloVehiculo: 0,//carModel
   marcaVehiculo: 0,//modelMake
   categoria: 0,//category
-  tipoVehiculo:0,//modelType
+  tipoVehiculo: 0,//modelType
 });
 
 let dataJSON;
@@ -92,9 +97,36 @@ onBeforeMount(async () => {
   tipoVehiculo.value = dataJSON.carTypes;
 });
 
-const addProduct = async () => {
-  console.log(producto.value);
+
+const { mutate: data } = useMutation(ADD_PRODUCT_QUERY);
+const newProduct = async () => {
+  producto.value.categoria = producto.value.categoria.id
+  producto.value.marcaVehiculo = producto.value.marcaVehiculo.make.id
+  producto.value.modeloVehiculo = producto.value.modeloVehiculo.id
+  producto.value.tipoVehiculo = producto.value.tipoVehiculo.id
+
+  try {
+    const response = await data({
+      imagen: producto.value.imagen,
+      precio: producto.value.precio,
+      nombre: producto.value.nombre,
+      cantidad: producto.value.cantidad,
+      tipoArticulo: producto.value.tipoArticulo,
+      descripcion: producto.value.descripcion,
+      categoria: producto.value.categoria,
+      modeloVehiculo: producto.value.modeloVehiculo,
+      tipoVehiculo: producto.value.tipoVehiculo,
+      marcaVehiculo: producto.value.marcaVehiculo
+    });
+    router.push({ name: "productdashboard" });
+  } catch (error) {
+    console.log(error)
+  }
 };
+
+
+
+
 
 const imageComputed = computed(() => producto.value.imagen);
 
@@ -106,116 +138,50 @@ const imageComputed = computed(() => producto.value.imagen);
     <div class="grid grid-cols-4 h-screen pt-5 bg-gray-100">
       <DashboardAside></DashboardAside>
       <div class="col-span-2 p-6 bg-white rounded-lg shadow-lg relative">
-        <div
-          class="absolute top-0 right-0 mt-4 mr-4 w-40 h-40 border border-solid border-sky-700 rounded-lg shadow-md"
-        >
-          <img
-            :src="imageComputed"
-            alt="Imagen del producto"
-            class="w-full h-full object-cover rounded-lg"
-          />
+        <div class="absolute top-0 right-0 mt-4 mr-4 w-40 h-40 border border-solid border-sky-700 rounded-lg shadow-md">
+          <img :src="imageComputed" alt="Imagen del producto" class="w-full h-full object-cover rounded-lg" />
         </div>
         <h1 class="text-2xl font-semibold mb-6">Nuevo producto</h1>
-        <form @submit.prevent="addProduct()" class="space-y-6">
+        <form @submit.prevent="newProduct()" class="space-y-6">
           <div>
-            <label
-              for="nombre-articulo"
-              class="block text-sm font-medium text-gray-700"
-              >Nombre articulo</label
-            >
-            <InputText
-              type="text"
-              v-model="producto.nombre"
-              id="nombre-articulo"
-              class="w-2/3 border-gray-300 rounded-md shadow-sm"
-            />
+            <label for="nombre-articulo" class="block text-sm font-medium text-gray-700">Nombre articulo</label>
+            <InputText type="text" v-model="producto.nombre" id="nombre-articulo"
+              class="w-2/3 border-gray-300 rounded-md shadow-sm" />
           </div>
           <div>
-            <label
-              for="descripcion"
-              class="block text-sm font-medium text-gray-700"
-              >Descripcion</label
-            >
-            <Textarea
-              v-model="producto.descripcion"
-              id="descripcion"
-              class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
-              autoResize
-            />
+            <label for="descripcion" class="block text-sm font-medium text-gray-700">Descripcion</label>
+            <Textarea v-model="producto.descripcion" id="descripcion"
+              class="mt-1 w-full border-gray-300 rounded-md shadow-sm" autoResize />
           </div>
           <div class="flex space-x-4">
             <div>
-              <label for="price" class="block text-sm font-medium text-gray-700"
-                >Precio</label
-              >
-              <InputNumber
-                v-model="producto.precio"
-                inputId="integeronly"
-                suffix="COP"
-                id="price"
-                class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
-              />
+              <label for="price" class="block text-sm font-medium text-gray-700">Precio</label>
+              <InputNumber v-model="producto.precio" inputId="integeronly" suffix="COP" id="price"
+                class="mt-1 w-full border-gray-300 rounded-md shadow-sm" />
             </div>
             <div>
-              <label
-                for="cantidad"
-                class="block text-sm font-medium text-gray-700"
-                >Cantidad</label
-              >
-              <InputNumber
-                v-model="producto.cantidad"
-                inputId="minmax"
-                :min="0"
-                :max="100"
-                id="cantidad"
-                class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
-              />
+              <label for="cantidad" class="block text-sm font-medium text-gray-700">Cantidad</label>
+              <InputNumber v-model="producto.cantidad" inputId="minmax" :min="0" :max="100" id="cantidad"
+                class="mt-1 w-full border-gray-300 rounded-md shadow-sm" />
             </div>
           </div>
 
           <div>
-            <label
-              for="imagen-producto"
-              class="block text-sm font-medium text-gray-700"
-              >Imagen</label
-            >
-            <Textarea
-              v-model="producto.imagen"
-              id="imagen-producto"
-              class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
-              autoResize
-            />
+            <label for="imagen-producto" class="block text-sm font-medium text-gray-700">Imagen</label>
+            <Textarea v-model="producto.imagen" id="imagen-producto"
+              class="mt-1 w-full border-gray-300 rounded-md shadow-sm" autoResize />
           </div>
           <div class="flex space-x-4">
             <div class="w-1/2">
-              <label
-                for="marca-vehiculo"
-                class="block text-sm font-medium text-gray-700"
-                >Marca Vehiculo</label
-              >
-              <Select
-                v-model="producto.marcaVehiculo"
-                :options="marca"
-                optionLabel="name"
-                placeholder="Marca"
-                class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
-                id="marca-vehiculo"
-              />
+              <label for="marca-vehiculo" class="block text-sm font-medium text-gray-700">Marca Vehiculo</label>
+              <Select v-model="producto.marcaVehiculo" :options="marca" optionLabel="name" placeholder="Marca"
+                class="mt-1 w-full border-gray-300 rounded-md shadow-sm" id="marca-vehiculo" />
             </div>
             <div class="w-1/2">
-              <label
-                for="modelo-vehiculo"
-                class="block text-sm font-medium text-gray-700"
-                >Modelo Vehiculo</label
-              >
-              <Select
-                v-model="producto.modeloVehiculo"
-                :options="modelo"
-                optionLabel="name"
-                placeholder="Modelo vehiculo"
-                class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
-                id="modelo-vehiculo"
-              />
+              <label for="modelo-vehiculo" class="block text-sm font-medium text-gray-700">Modelo Vehiculo</label>
+              <Select v-model="producto.modeloVehiculo" :options="modelo" optionLabel="name"
+                placeholder="Modelo vehiculo" class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
+                id="modelo-vehiculo" />
               <p class="mt-1 text-sm text-blue-500">
                 N° de modelo:
                 <!--{{  }} -->
@@ -224,64 +190,34 @@ const imageComputed = computed(() => producto.value.imagen);
           </div>
           <div class="flex space-x-4">
             <div class="w-1/2">
-              <label
-                for="categoria-vehiculo"
-                class="block text-sm font-medium text-gray-700"
-                >Categoria</label
-              >
-              <Select
-                v-model="producto.categoria"
-                :options="categoria"
-                optionLabel="name"
-                placeholder="Categoria producto"
-                class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
-                id="categoria-vehiculo"
-              />
+              <label for="categoria-vehiculo" class="block text-sm font-medium text-gray-700">Categoria</label>
+              <Select v-model="producto.categoria" :options="categoria" optionLabel="name"
+                placeholder="Categoria producto" class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
+                id="categoria-vehiculo" />
               <p class="mt-1 text-sm text-green-500">
                 Descuento! {{ producto.categoria.discount }}%
               </p>
             </div>
 
             <div class="w-1/2">
-              <label
-                for="tipo-articulo"
-                class="block text-sm font-medium text-gray-700"
-                >Tipo articulo</label
-              >
-              <Select
-                v-model="producto.tipoArticulo"
-                :options="tipo"
-
-                placeholder="Tipo de articulo"
-                class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
-                id="tipo-articulo"
-              />
+              <label for="tipo-articulo" class="block text-sm font-medium text-gray-700">Tipo articulo</label>
+              <Select v-model="producto.tipoArticulo" :options="tipo" placeholder="Tipo de articulo"
+                class="mt-1 w-full border-gray-300 rounded-md shadow-sm" id="tipo-articulo" />
             </div>
 
           </div>
 
           <div class="flex space-x-4">
             <div class="w-1/2">
-              <label
-                for="tipo-vehiculo"
-                class="block text-sm font-medium text-gray-700"
-                >Tipo vehiculo</label
-              >
-              <Select
-                v-model="producto.tipoVehiculo"
-                :options="tipoVehiculo"
-                optionLabel="name"
-                placeholder="Tipo de vehiculo"
-                class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
-                id="tipo-vehiculo"
-              />
+              <label for="tipo-vehiculo" class="block text-sm font-medium text-gray-700">Tipo vehiculo</label>
+              <Select v-model="producto.tipoVehiculo" :options="tipoVehiculo" optionLabel="name"
+                placeholder="Tipo de vehiculo" class="mt-1 w-full border-gray-300 rounded-md shadow-sm"
+                id="tipo-vehiculo" />
             </div>
           </div>
           <div>
-            <button
-              type="submit"
-              class="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
+            <button type="submit"
+              class="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
               Agregar
             </button>
           </div>
